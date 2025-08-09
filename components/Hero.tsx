e38a78b9b316d7e81c4021d5bfa8c386cb8a92cf
aatika-hakim <aatikahakim90@gmail.com>
2025-08-09 18:17:44 +0500

    added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,82 @@
-import React from 'react'
+import Image from 'next/image'
+import Button from './Button'
 
 const Hero = () => {
   return (
-    <section
-  className= "h-2xl overflow-hidden bg-[url(https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?fm=jpg)] bg-cover bg-top bg-no-repeat"
->
-  <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-    <div className="text-center ltr:sm:text-left rtl:sm:text-right">
-      <h2 className="text-4xl font-bold text-white sm:text-3xl md:text-5xl">
-        Discover Breathtaking Destinations
-      </h2>
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
 
-      <p className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-xl md:leading-relaxed">
-        From tropical beaches to hidden mountain trails — plan your dream vacation with curated travel guides, expert tips, and more.
-      </p>
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image 
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
+        </p>
 
-      <div className="mt-4 sm:mt-8">
-        <a
-          href="#"
-          className="inline-block bg-teal-600 px-12 py-4 text-sm font-medium text-white transition hover:bg-teal-700 focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
-        >
-          Start Exploring
-        </a>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5).fill(1).map((_, index) => (
+              <Image 
+                src="/star.svg"
+                key={index}
+                alt="star"
+                width={24}
+                height={24}
+              />
+            ))}
+          </div>
+
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
+          </p>
+        </div>
+
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button 
+            type="button" 
+            title="Download App" 
+            variant="btn_green" 
+          />
+          <Button 
+            type="button" 
+            title="How we work?" 
+            icon="/play.svg"
+            variant="btn_white_text" 
+          />
+        </div>
       </div>
-    </div>
-  </div>
-</section>
 
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+
+           <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Aguas Calientes</p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 mi</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.040 km</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,114 +1,41 @@
-import Link from "next/link";
-import React from "react";
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+import Button from "./Buttons"
 
 const Navbar = () => {
   return (
-    <nav className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
-          <div className="md:flex md:items-center md:gap-12">
-            <Link className="block text-teal-600 dark:text-teal-600 text-xl" href="#">
-              Travel Anywhere
-            </Link>
-          </div>
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={74} height={29} />
+      </Link>
 
-          <div className="hidden md:block">
-            <nav aria-label="Global">
-              <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="#"
-                  >
-                    Destinations
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="#"
-                  >
-                    Flights
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="#"
-                  >
-                    Hotels
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="#"
-                  >
-                    Packages
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="#"
-                  >
-                    Experiences
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="#"
-                  >
-                    Blog
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
 
-          <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <Link
-                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm dark:hover:bg-teal-500"
-                href="#"
-              >
-                Sign In
-              </Link>
-
-              <div className="hidden sm:flex">
-                <Link
-                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Join
-                </Link>
-              </div>
-            </div>
-
-            <div className="block md:hidden">
-              <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
+      <div className="lg:flexCenter hidden">
+        <Button 
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
       </div>
-    </nav>
-  );
-};
 
-export default Navbar;
+      <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  )
+}
+
+export default Navbar
